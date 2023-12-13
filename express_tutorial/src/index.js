765d3a5dcfa741eeef77f128de66967e305acfe4
Adeola Adesina <adeolaadesina@gmail.com>
2023-12-13 14:38:36 +0100

    Update index.js

--- express_tutorial/src/index.js
+++ express_tutorial/src/index.js
@@ -5,4 +5,24 @@ const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running express server on Port ${PORT}!`))
 
+//But we don't have any route defined, we cannot visit any route currently.const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.listen(PORT, () => console.log(`Running express server on Port ${PORT}!`))
+
 //But we don't have any route defined, we cannot visit any route currently.
+
+app.get('/groceries', (req, res) => {
+    res.send([
+        {
+        item: 'milk',
+        quantity: 2,
+        },
+        {
+        item: 'cereal',
+        quantity: 4,
+        },
+    ]);
+}); 
